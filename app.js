ee32185430f80c96d2430c6204154c317735ad8d
Ghostigital <59442842+Ghostigital@users.noreply.github.com>
2023-05-11 18:46:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+function celsiusToFahrenheit(celsius) {
+    let fahrenheit = Math.floor(celsius * (9/5) + 32);
+    console.log(celsius + " degrees Celsius is equal to " + fahrenheit + " degrees Fahrenheit.");
+}
+//Type your value in the brackets to see the conversion in the console
+celsiusToFahrenheit(11);
